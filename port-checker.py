254b9f17794e646e1cdc05f38111f9272b940051
amirak <dashti.amir2752@gmail.com>
2022-02-17 16:09:21 +0100

    delete

--- port-checker.py
+++ port-checker.py
@@ -5,7 +5,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
     s.bind(("127.0.0.1", 3000))
 except socket.error as e:
-        os.system('sudo kill -9 $(sudo lsof -t -i:3000) && pm2 stop ronia')
+        os.system('sudo kill -9 $(sudo lsof -t -i:3000) && pm2 delete ronia')
         print(e.errno)
         sys.exit(0)
 
