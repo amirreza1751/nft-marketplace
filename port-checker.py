e2cdd88eb72e940b35fdd16414422c5bc6bfdf5b
amirak <dashti.amir2752@gmail.com>
2022-02-17 15:53:42 +0100

    add port checker script

---
+++ port-checker.py
@@ -0,0 +1,12 @@
+import os
+import sys
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    s.bind(("127.0.0.1", 3000))
+except socket.error as e:
+        os.system('sudo kill -9 $(sudo lsof -t -i:3000)')
+        print(e.errno)
+        sys.exit(0)
+
+s.close()
